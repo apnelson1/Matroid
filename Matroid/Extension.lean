3c60d65f2b20a9737d0c0f19ac7033714d1c690e
Mattias Ehatamm <mehatamm@cogradmac309.nexus.uwaterloo.ca>
2023-09-25 16:59:01 -0400

    almost done

--- Matroid/Extension.lean
+++ Matroid/Extension.lean
@@ -23,37 +23,20 @@ def ParallelExt (M : Matroid α) (e : α) (S : Set α) (S_j : Disjoint S M.E): M
     apply Or.inl (Indep.subset h_Indep _)
     apply subset_union_of_subset_right (subset_diff.2 ⟨Isub, _⟩)
     rwa [disjoint_singleton_right]
-    · right
-      refine ⟨not_mem_subset Isub e_ni_I, ?_⟩
-      use f
-      refine ⟨fmI, ⟨f_s, ⟨?_, ?_⟩⟩⟩
-      apply disjoint_of_subset_right _ I_d
-      apply diff_subset_diff_left Isub
-      apply Indep.subset h_Indep _
-      apply insert_subset_insert _
-      apply diff_subset_diff_left Isub
-    · left
-      refine ⟨disjoint_of_subset_right (subset_diff_singleton Isub fmI) I_d, ?_⟩
-      apply Indep.subset h_Indep _
-      apply subset_union_of_subset_right (subset_diff_singleton Isub fmI)
   )   
   (by
-    rintro I X (⟨I_disj, I_ind⟩ | ⟨e_nI, f, f_I, f_S, f_disj, f_ind⟩) I_not_max X_max
+    rintro I X (I_ind | ⟨e_nI, f, f_I, f_S, f_disj, f_ind⟩) I_not_max X_max
     dsimp
     rw [mem_maximals_setOf_iff] at I_not_max X_max
     push_neg at I_not_max
     have I_nB : ¬ M.Base I
     · sorry 
-    obtain ⟨(⟨X_disj, X_ind⟩ | ⟨e_nX, f, f_X, f_S, f_disj, f_ind⟩), X_max⟩ := X_max
+    obtain ⟨(X_ind | ⟨e_nX, f, f_X, f_S, f_disj, f_ind⟩), X_max⟩ := X_max
     have X_B : M.Base X
     · sorry
     obtain ⟨e, e_Xdiff, e_Ind⟩ := Indep.exists_insert_of_not_base I_ind I_nB X_B
     use e
-    refine ⟨e_Xdiff, Or.inl ⟨?_, e_Ind⟩⟩
-    apply Disjoint.union_right _ I_disj
-    apply disjoint_of_subset_right _ X_disj
-    apply singleton_subset_iff.2 _
-    apply mem_of_mem_diff e_Xdiff
+    refine ⟨e_Xdiff, Or.inl e_Ind⟩
     have X_B: M.Base (insert e (X \ {f}))
     · sorry
     obtain ⟨x, x_Xdiff, x_Ind⟩ := Indep.exists_insert_of_not_base I_ind I_nB X_B
@@ -64,7 +47,8 @@ def ParallelExt (M : Matroid α) (e : α) (S : Set α) (S_j : Disjoint S M.E): M
     use f
     rw [mem_diff]
     refine ⟨⟨f_X, ?_⟩, ?_⟩
-    apply disjoint_left.1 I_disj f_S
+    apply disjoint_left.1 _ f_S
+    apply disjoint_of_subset_right (by aesop_mat) S_j
     right
     constructor
     rw [mem_insert_iff]
@@ -79,23 +63,15 @@ def ParallelExt (M : Matroid α) (e : α) (S : Set α) (S_j : Disjoint S M.E): M
     apply insert_diff_of_mem _ (mem_singleton _)
     refine ⟨Set.mem_insert _ _, f_S, ?_, ?_⟩
     rw [fif_eq]
-    apply disjoint_of_subset_right _ I_disj
-    apply diff_subset _ _
+    apply disjoint_of_subset_right (subset_trans (diff_subset _ _) (by aesop_mat)) S_j
+    rw [fif_eq, ←x_eq_e]
     apply Indep.subset x_Ind
-    rw [x_eq_e, fif_eq]
     apply insert_subset_insert (diff_subset _ _)
     use x
     rw [mem_diff] at *
-    constructor
-    obtain h := mem_of_mem_insert_of_ne x_Xdiff.1 x_eq_e
-    exact ⟨mem_of_mem_diff h, x_Xdiff.2⟩
-    left
-    refine ⟨?_, x_Ind⟩
-    apply Disjoint.union_right
-    apply disjoint_singleton_right.2
-    apply disjoint_right.1 f_disj
-    apply mem_of_mem_insert_of_ne x_Xdiff.1 x_eq_e
-    exact I_disj
+    refine' ⟨⟨mem_of_mem_diff (mem_of_mem_insert_of_ne x_Xdiff.1 x_eq_e), x_Xdiff.2⟩, _⟩
+    exact Or.inl x_Ind
+
     -- part 2
     dsimp
     rw [mem_maximals_setOf_iff] at I_not_max X_max
@@ -103,7 +79,7 @@ def ParallelExt (M : Matroid α) (e : α) (S : Set α) (S_j : Disjoint S M.E): M
     · sorry 
     have I_nB₂ : ¬ M.Base I
     · sorry
-    obtain ⟨(⟨X_disj, X_ind⟩ | ⟨e_nX, f, f_X, f_S, f_disj, f_ind⟩), X_max⟩ := X_max
+    obtain ⟨(X_ind | ⟨e_nX, l, l_X, l_S, l_disj, l_ind⟩), X_max⟩ := X_max
     have e_ne_f: e ≠ f
     · intro h_f
       apply e_nI
@@ -112,34 +88,64 @@ def ParallelExt (M : Matroid α) (e : α) (S : Set α) (S_j : Disjoint S M.E): M
     have X_B : M.Base X
     · sorry
     obtain ⟨x, x_Xdiff, x_ind⟩ := Indep.exists_insert_of_not_base f_ind I_nB X_B
-    by_cases x_eq_f : x = f
-    rw [x_eq_f] at x_ind
-    have I_ind : M.Indep I
-    · apply Indep.subset x_ind    
-      rw [insert_def]
-      intro y h_y
-      by_cases y_f : y = f
-      exact (Or.inl y_f)
-      right
-      apply mem_insert_of_mem
-      exact ⟨h_y, y_f⟩
-    obtain ⟨y, y_Xdiff, y_ind⟩ := Indep.exists_insert_of_not_base I_ind I_nB₂ X_B
-    use y
-    refine ⟨y_Xdiff, Or.inl ⟨?_, y_ind⟩⟩
-    apply disjoint_of_subset_right (y_ind.subset_ground) S_j
+    have x_ne_f : x ≠ f
+    · intro h_f
+      rw [h_f] at x_ind
+      apply disjoint_left.1 S_j f_S
+      rw [←singleton_subset_iff]
+      apply Indep.subset_ground (Indep.subset x_ind _)
+      simp
     use x
-    refine' ⟨_, _⟩
+    refine' ⟨_, Or.inr _⟩
     rw [mem_diff] at x_Xdiff ⊢
-    refine' ⟨x_Xdiff.1, _⟩
-    intro x_I
+    refine' ⟨x_Xdiff.1, fun h_f ↦ _⟩
     apply x_Xdiff.2
     apply mem_union_right
-    rw [mem_diff]
-    refine' ⟨x_I, _⟩
-    rw [mem_singleton_iff]
-    exact x_eq_f
-    left
-
+    exact (mem_diff x).2 ⟨h_f, x_ne_f⟩
+    have e_ne_x : e ≠ x
+    · intro h_f
+      rw [mem_diff] at x_Xdiff
+      apply x_Xdiff.2
+      rw [mem_insert_iff]
+      exact (Or.inl h_f.symm)
+    rw [mem_insert_iff]
+    push_neg
+    refine' ⟨⟨e_ne_x, e_nI⟩, f, mem_insert_of_mem x f_I, f_S, _⟩
+    rw [insert_comm _ _ _, insert_diff_singleton_comm x_ne_f _] at x_ind
+    refine' ⟨_, x_ind⟩
+    rw [←insert_diff_singleton_comm x_ne_f _]
+    apply Disjoint.union_right _ f_disj
+    apply disjoint_singleton_right.2
+    apply disjoint_right.1 S_j _
+    rw [←singleton_subset_iff]
+    apply Indep.subset_ground (Indep.subset X_ind _)
+    rw [singleton_subset_iff]
+    exact ((mem_diff x).1 x_Xdiff).1
+    have X_B : M.Base (insert e (X \ {l}))
+    · sorry
+    obtain ⟨x, x_Xdiff, x_ind⟩ := Indep.exists_insert_of_not_base f_ind I_nB X_B
+    use x
+    rw [mem_diff] at x_Xdiff ⊢
+    have x_in_X : x ∈ X
+    · apply mem_of_mem_diff (mem_of_mem_insert_of_ne x_Xdiff.1 _)
+      intro h_f
+      apply x_Xdiff.2
+      rw [h_f]
+      exact mem_insert e _
+    have x_notin_I : x ∉ I
+    · intro h_f
+      apply x_Xdiff.2
+      apply mem_insert_of_mem
+      rw [mem_diff, mem_singleton_iff]
+      refine' ⟨h_f, fun h_f₂ ↦ _⟩
+      apply disjoint_left.1 S_j f_S
+      rw [←h_f₂]
+      rw [←singleton_subset_iff]
+      apply Indep.subset_ground (Indep.subset x_ind _)
+      rw [singleton_subset_iff]
+      exact mem_insert x _
+    
+      
 
 
 
