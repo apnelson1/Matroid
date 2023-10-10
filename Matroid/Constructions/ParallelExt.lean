9f87399e7fd8e6254ac7d02252ab09ff826e7b57
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2023-10-10 09:32:21 -0400

    change def

--- Matroid/Constructions/ParallelExt.lean
+++ Matroid/Constructions/ParallelExt.lean
@@ -6,16 +6,31 @@ open Set
 
 variable {α β : Type _}
 
-def parallel_preimage (M : Matroid β) (E : Set α) (f : α → β) : Matroid α := matroid_of_indep
-  E 
-  ( fun I ↦ M.Indep (f '' I) ∧ InjOn f I )
-  ( by simp )
-  ( fun I J ⟨h, h'⟩ hIJ ↦ ⟨h.subset (image_subset _ hIJ), InjOn.mono hIJ h'⟩ )
+def parallel_preimage (M : Matroid β) (E : Set α) (f : α → β) : Matroid α := matroid_of_base
   sorry 
   sorry
   sorry 
+  sorry 
+  sorry 
+  sorry 
+
+
+-- def parallel_preimage (M : Matroid β) (E : Set α) (f : α → β) : Matroid α := matroid_of_indep
+--   E 
+--   ( fun I ↦ M.Indep (f '' I) ∧ InjOn f I ∧ I ⊆ E )
+--   ( by simp )
+--   ( fun I J ⟨h, h', hE⟩ hIJ ↦ ⟨h.subset (image_subset _ hIJ), InjOn.mono hIJ h', hIJ.trans hE⟩ )
+--   ( by
+--     rintro I J hI  hImax hJmax
+
+--     --
+--     -- simp [mem_maximals_iff, iff_true_intro hI, iff_true_intro hIinj, iff_true_intro hIE] at hImax
+    
+--   )
+--   sorry
+--   ( fun I h ↦ h.2.2 )
 
 @[simp] theorem parallel_preimage_indep_iff {M : Matroid β} {E : Set α} {f : α → β} {I : Set α} : 
-    (M.parallel_preimage E f).Indep I ↔ (M.Indep (f '' I) ∧ InjOn f I) := by 
-  simp [parallel_preimage]
+    (M.parallel_preimage E f).Indep I ↔ (M.Indep (f '' I) ∧ InjOn f I ∧ I ⊆ E) := by 
+  sorry 
   
