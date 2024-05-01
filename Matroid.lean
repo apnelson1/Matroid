9a914c344087c2e455407f486858a99c4d87eeb2
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-05-01 08:07:50 -0400

    wip

--- Matroid.lean
+++ Matroid.lean
@@ -1,5 +1,5 @@
 -- import Matroid.Simple
-import Matroid.Representation.Basic
+-- import Matroid.Representation.Basic
 import Matroid.ForMathlib.ForMathlib
 import Matroid.Modular
 import Matroid.Minor.Rank
