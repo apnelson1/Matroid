63ea9a794cfd6987f85261f06568398478f813ff
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-06-28 16:11:52 -0400

    wip

--- Matroid/Constructions/CircuitAxioms.lean
+++ Matroid/Constructions/CircuitAxioms.lean
@@ -53,22 +53,20 @@ lemma Finset.sdiff_erase_not_mem {s t : Finset α} {a : α} (h : a ∉ s) : s \ 
 
 lemma Indep.aug {M : FinsetCircuitMatroid α} (hI : M.Indep I) (hJ : M.Indep J)
     (hIJ : I.card < J.card) : ∃ e ∈ J, e ∉ I ∧ M.Indep (insert e I) := by
-  obtain ⟨K, ⟨hK_subset, hK_indep, hK_card⟩, hK_min⟩ := by
-    have h_finite : {S | S ⊆ I ∪ J ∧ M.Indep S ∧ I.card < S.card}.Finite := by
-      refine (finite_toSet <| (I ∪ J).powerset).subset ?_
-      intro S hS; simp only [mem_coe, mem_powerset]
-      exact hS.1
-    exact Set.Finite.exists_minimal_wrt (card <| I \ ·)
-      {S | S ⊆ I ∪ J ∧ M.Indep S ∧ I.card < S.card} h_finite
-      (by use J; exact ⟨subset_union_right, hJ, hIJ⟩)
-  by_cases hIK : (I \ K).Nonempty; swap
-  · simp only [not_nonempty_iff_eq_empty, sdiff_eq_empty_iff_subset] at hIK
-    obtain ⟨e, heK, heI⟩ :=
-      exists_of_ssubset <| Finset.ssubset_of_subset_of_card_lt_card hK_card hIK
-    use e; refine ⟨?_, heI, Indep.subset hK_indep (insert_subset heK hIK)⟩
-    obtain _ | _ := mem_union.mp <| hK_subset heK
-    <;> tauto
-  obtain ⟨e, he⟩ := hIK
+  let T := {S | S ⊆ I ∪ J ∧ M.Indep S ∧ I.card < S.card}
+  have hfin : T.Finite :=
+    (finite_toSet <| (I ∪ J).powerset).subset fun S hS ↦ by simp only [hS.1, mem_coe, mem_powerset]
+  have hne : T.Nonempty := ⟨J, subset_union_right, hJ, hIJ⟩
+  obtain ⟨K, ⟨hK_subset, hK_indep, hK_card⟩, hK_min⟩ :=
+    Set.Finite.exists_minimal_wrt (card <| I \ ·) _ hfin hne
+
+  obtain (h_empty | ⟨e, he⟩) := (I \ K).eq_empty_or_nonempty
+  · have hssu : I ⊂ K := (sdiff_eq_empty_iff_subset.1 h_empty).ssubset_of_ne
+      (by rintro rfl; simp at hK_card)
+    obtain ⟨e, heK, heI⟩ := exists_of_ssubset hssu
+    have heJ := (mem_union.1 (hK_subset heK)).elim (False.elim ∘ heI) id
+    refine ⟨e, heJ, heI, hK_indep.subset <| insert_subset heK hssu.subset⟩
+
   obtain ⟨heI, heK⟩ := Finset.mem_sdiff.mp he
   have hKfe : ∀ f ∈ K \ I, ∃ C ⊆ ((insert e K).erase f), M.Circuit C := by
     intro f hf
@@ -135,7 +133,7 @@ lemma Indep.subset_ground {M : FinsetCircuitMatroid α} (hI : M.Indep I) : ↑I 
   (indep_aug := fun _ _ hI hJ ↦ hI.aug hJ)
   (subset_ground := fun _ hI ↦ hI.subset_ground)
 
-@[simp] theorem finsetCircuitMatroid_circuit_iff_matroid_circuit (M : FinsetCircuitMatroid α)
+@[simp] theorem matroid_circuit_iff (M : FinsetCircuitMatroid α)
     {I : Finset α} : M.matroid.Circuit I ↔ M.Circuit I := by
   unfold Matroid.Circuit Matroid.Dep
   simp only [matroid_Indep, matroid_E, IndepMatroid.ofFinset_indep]
@@ -145,22 +143,24 @@ lemma Indep.subset_ground {M : FinsetCircuitMatroid α} (hI : M.Indep I) : ↑I 
     push_neg at h_indep
     specialize h_indep h_subset
     obtain ⟨C, hC_subset, hC⟩ := h_indep
-    have h_bad : ¬M.Indep C := by
-      rw [M.indep_iff];
-      intro ⟨_, h_bad⟩
-      exact h_bad (subset_refl C) hC
+    have h_bad : ¬M.Indep C := by simp
+      [M.indep_iff, show ∃ x ⊆ C, M.Circuit x from ⟨C, Subset.rfl, hC⟩]
     rw [← IndepMatroid.ofFinset_indep M.E M.Indep M.indep_empty (fun _ _ hJ ↦ hJ.subset)
       (fun _ _ hI hJ ↦ hI.aug hJ) (fun _ hI ↦ hI.subset_ground)] at h_bad
-    have h := h_min ⟨h_bad, (Finset.coe_subset.mpr hC_subset).trans h_subset⟩
+    specialize h_min ⟨h_bad, (Finset.coe_subset.mpr hC_subset).trans h_subset⟩
       <| Finset.coe_subset.mpr hC_subset
-    simp only [coe_subset] at h
-    rwa [h.antisymm hC_subset]
+    simp only [coe_subset] at h_min
+    rwa [h_min.antisymm hC_subset]
   refine ⟨⟨?_, ?_⟩, fun C ⟨hC, hC'⟩ hC_subset ↦ ?_⟩
   · simp only [IndepMatroid.ofFinset_indep, M.indep_iff]
     push_neg; intro _; use I
   · exact M.circuit_subset_ground h
-  dsimp only at hC_subset ⊢
-  -- #check IndepMatroid.ofFinset_indep M.E
-  -- simp [IndepMatroid.ofFinset_indep M.E] at hC
-  sorry
+  obtain (rfl | hssu) := hC_subset.eq_or_ssubset
+  · rfl
+  obtain ⟨D, hDC, hD⟩ : ∃ x, ↑x ⊆ C ∧ ¬M.Indep x := by simpa [IndepMatroid.ofFinset] using hC
+  replace hD := show (D : Set α) ⊆ M.E → ∃ x ⊆ D, M.Circuit x by simpa [indep_iff] using hD
+  obtain ⟨C₁, hC₁D, hC₁⟩ := hD (hDC.trans hC')
+  have hssu : C₁ ⊂ I := hC₁D.trans_ssubset (Finset.coe_ssubset.1 (hDC.trans_ssubset hssu))
+  exact False.elim <| hssu.ne <| M.circuit_antichain.eq hC₁ h hssu.subset
+
 end FinsetCircuitMatroid
