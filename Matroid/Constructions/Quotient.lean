36db7b156b60378fff1445ce157d4593a1f57fb9
fair8 <118382318+fair8@users.noreply.github.com>
2024-07-25 10:43:05 -0400

    WIP

--- Matroid/Constructions/Quotient.lean
+++ Matroid/Constructions/Quotient.lean
@@ -85,7 +85,61 @@ theorem Quotient.relRank_le {M₁ M₂: Matroid α} (hQ : M₂ ≤q M₁) {X : S
   refine le_top.trans hinf
 termination_by M₁.relRank X Y
 
-theorem Flat_covers {M₁ M₂ : Matroid α} {X Y : Set α}
+lemma Insert_not_closure {M : Matroid α} {X : Set α }{x : α} (hx : x ∈ M.E) (hX : X ⊆ M.E) (hno : x ∈ M.closure X) :
+    M.relRank X (insert x X) = 0 := by
+  refine relRank_eq_zero_iff'.mpr ?_
+  have hxx : (insert x X) ∩ M.E = insert x X := by
+    refine inter_eq_left.mpr ?_
+    exact insert_subset hx hX
+  rw [hxx]
+  refine insert_subset_iff.mpr ?_
+  constructor
+  exact hno
+  exact subset_closure_of_subset' M (fun ⦃a⦄ a ↦ a) hX
+  --refine relRank_insert_eq_one ?he ?hX
+
+theorem Quo_3 {M₁ M₂ : Matroid α} {X: Set α} (hE : M₁.E = M₂.E) (hX: X ⊆ M₁.E)
+    (hYZ: ∀ Y Z, Z ⊆ Y → Y ⊆ M₁.E → M₂.relRank Z Y ≤ M₁.relRank Z Y ) :
+    M₁.closure X ⊆ M₂.closure X := by
+  have hXg: X = X ∩ M₂.E := by
+    refine left_eq_inter.mpr ?_
+    rw [hE] at hX
+    exact hX
+  have hXin : X ⊆ M₂.closure X := by
+    rw [hXg]
+    simp only [closure_inter_ground]
+    exact M₂.inter_ground_subset_closure X
+  have hFlat : M₁.Flat (M₂.closure X) := by
+    by_contra! hc
+    have hsu : M₂.closure X ⊆ M₁.E:= by
+      rw[hE]
+      exact closure_subset_ground M₂ X
+    have hex := exists_mem_closure_not_mem_of_not_flat hc hsu
+    obtain⟨e , he ⟩:= hex
+    have hee : e ∈ M₂.E \ M₂.closure (M₂.closure X) := by
+        refine (mem_diff e).mpr ?_
+        constructor
+        have hsue : M₁.closure (M₂.closure X) ⊆ M₂.E:= by
+          rw [hE.symm]
+          exact closure_subset_ground M₁ (M₂.closure X)
+        exact hsue (mem_of_mem_diff he)
+        simp only [closure_closure]
+        exact not_mem_of_mem_diff he
+    have hc2 : M₂.relRank (M₂.closure X) (insert e (M₂.closure X)) = 1 := by
+      have hXi: (M₂.closure X ⊆ M₂.E) := by exact closure_subset_ground M₂ X
+      exact relRank_insert_eq_one hee hXi
+    have hc1 : M₁.relRank (M₂.closure X) (insert e (M₂.closure X)) = 0 := Insert_not_closure (closure_subset_ground M₁ (M₂.closure X) (mem_of_mem_diff he)) hsu (mem_of_mem_diff he)
+    have hi : M₂.closure X ⊆ insert e (M₂.closure X) := subset_insert e (M₂.closure X)
+    have hhelp1 : e ∈ M₂.E := by exact mem_of_mem_diff hee
+    have he1 : e ∈  M₁.E := by rwa[hE.symm] at hhelp1
+    have hEi : insert e (M₂.closure X) ⊆ M₁.E := by exact insert_subset he1 hsu
+    have hcon:= hYZ (insert e (M₂.closure X)) (M₂.closure X) hi hEi
+    rw[hc1, hc2] at hcon
+    norm_num at hcon
+  exact closure_flat_idk X (M₂.closure X) M₁ hFlat hXin
+
+
+theorem Flat_covers {M₁ M₂ : Matroid α} {X Y : Set α} [FiniteRk M]
     (hYE : Y ⊆ M₁.E) (hX2: M₂.Flat X) (hco : Flat_Covers_Flat M₁ Y X) (hMX : M₁.relRank X (M₁.E)= M₂.relRank X (M₂.E) )
     (hQ' : Quotient' M₁ M₂):
     (Flat_Covers_Flat M₂ Y X) ∧ M₁.relRank Y (M₁.E)= M₂.relRank Y (M₁.E) := by
