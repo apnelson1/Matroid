15a657ddee3d3881b079e6828fe3cb4ddecfb27c
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2024-10-18 09:51:57 -0400

    wip

--- Matroid/Constructions/TwoSum.lean
+++ Matroid/Constructions/TwoSum.lean
@@ -5,7 +5,38 @@ namespace Matroid
 
 variable {α β : Type*} {e : α} {f : β}
 
-def TwoSum (M : Matroid α) (N : Matroid β) (e : α) (f : β) : Matroid (α ⊕ β) := by
+def TwoSum (M : Matroid α) (N : Matroid β) (e : α) (f : β) : Matroid (α ⊕ β) :=
   let MN := M.sum N
   let L : Set (α ⊕ β) := {.inl e, .inr f}
-  exact ((MN.projectBy <| ModularCut.principal MN L) ＼ L)
+  ((MN.projectBy <| ModularCut.principal MN L) ＼ L)
+
+-- def InternalTwoSumIndepMatroid (M N : Matroid α) (e : α) (hMN : M.E ∩ N.E = {e}) :
+--     IndepMatroid α where
+--   E := M.E ∪ N.E
+--   Indep I := I ⊆ M.E ∪ N.E ∧ M.Indep (I ∩ M.E) ∧ N.Indep (I ∩ N.E)
+--   indep_empty := sorry
+--   indep_subset := sorry
+--   indep_aug := sorry
+--   indep_maximal := sorry
+--   subset_ground := sorry
+
+-- def InternalTwoSum (M N : Matroid α) (e : α) (hMN : M.E ∩ N.E = {e}) : Matroid α :=
+--   (InternalTwoSumIndepMatroid M N e hMN).matroid
+
+-- @[simp] lemma internalTwoSum_indep_iff (M N : Matroid α) e hMN (I : Set α) :
+--     (InternalTwoSum M N e hMN).Indep I ↔ I ⊆ M.E ∪ N.E ∧ M.Indep (I ∩ M.E) ∧ N.Indep (I ∩ N.E) := by
+--   rfl
+
+-- def ModularSum (M N : Matroid α) (A : Set α) (h : M.E ∩ N.E = A) (hAMN : M ↾ A = N ↾ A) :
+--   Matroid α
+
+
+
+-- ( ........  e)    g     (f ........ )
+
+--         α       e  |      β        f  |   g
+-- [  * * * * * *  1  |   0 0 0 0 0 0 0  |   1   ]
+-- [  * * * * * *  3  |   0 0 0 0 0 0 0  |   3   ]
+-- [  0 0 0 0 0 0  0  |   * * * * * * 3  |   3t  ]
+-- [  0 0 0 0 0 0  0  |   * * * * * * 6  |   6t  ]
+-- [  0 0 0 0 0 0  0  |   * * * * * * 8  |   8t  ]
