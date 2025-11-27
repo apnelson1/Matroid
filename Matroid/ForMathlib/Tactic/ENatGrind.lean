98d5ee11d4aafab384ee704b53659d8647aa5614
Peter Nelson <71660771+apnelson1@users.noreply.github.com>
2025-11-27 16:24:22 -0500

    wip

--- Matroid/ForMathlib/Tactic/ENatGrind.lean
+++ Matroid/ForMathlib/Tactic/ENatGrind.lean
@@ -6,25 +6,9 @@ set_option linter.style.longLine false
 
 namespace ENat.Grind
 
-variable {a b : ℕ∞} {r : ℕ → ℕ → Prop}
+variable {a b c : ℕ∞} {r : ℕ → ℕ → Prop}
 
-/- These lemmas look for low-hanging fruit in the existing `ENat` (in)equalities to deduce
-logical information about variables being `⊤` or `0`. For instance, `a + b ≤ a + c` will
-be simplified to `b ≤ c ∨ a = ⊤`.
-
-This is run as a preprocessing step; after the loop in the tactic has started running,
-the hypotheses will not be in a form that these lemmas can see, so there is no point trying
-them again. Rephrasing them in a form that they can be applied in the middle of the run
-would probably be a modest speedup, but what they are achieving will be cleaned up by `omega`
-and brute force anyway. -/
-attribute [enat_grind_presimp] ENat.add_le_right_iff ENat.add_le_left_iff ENat.add_eq_right_iff
-  ENat.add_eq_left_iff ENat.eq_left_add_iff ENat.eq_right_add_iff ENat.lt_add_left_iff
-  ENat.lt_add_right_iff ENat.add_eq_add_left_iff ENat.add_eq_add_right_iff
-  ENat.add_le_add_left_iff ENat.add_le_add_right_iff ENat.add_lt_add_left_iff
-  ENat.add_lt_add_right_iff self_le_add_right self_le_add_left
-
-/- After the preprocessing step above,
-the idea is that we take every `ℕ∞` (inequality) `h`, and rephrase it in the form
+/- The idea is that we take every `ℕ∞` (inequality) `h`, and rephrase it in the form
 `(h ∧ h₀) ∨ h₁`, where `h₀` is a statement that to the effect that all terms appearing in `h`
 are finite, and `h₁` is a boolean combination of equalities of the form `_ = 0` and `_ = ⊤`.
 
@@ -93,6 +77,19 @@ structure Rel (r : ℕ → ℕ → Prop) (a b : ℕ∞) : Prop where
   ne_right : b ≠ ⊤
   rel : r a.toNat b.toNat
 
+
+lemma rel_le_iff : Rel (· ≤ ·) a b ↔ a ≤ b ∧ IsNat a ∧ IsNat b := by
+  cases a with cases b with simp [rel_iff, IsNat]
+
+lemma rel_eq_iff : Rel (· = ·) a b ↔ a = b ∧ IsNat a ∧ IsNat b := by
+  cases a with cases b with simp [rel_iff, IsNat]
+
+lemma rel_lt_iff : Rel (· < ·) a b ↔ a < b ∧ IsNat a ∧ IsNat b := by
+  cases a with cases b with simp [rel_iff, IsNat]
+
+lemma rel_ne_iff : Rel (· ≠ ·) a b ↔ a ≠ b ∧ IsNat a ∧ IsNat b := by
+  cases a with cases b with simp [rel_iff, IsNat]
+
 -- simp lemmas for the interaction of `Rel` with coercions, infinities, numerals, zeroes and ones.
 lemma rel_swap : Rel r a b ↔ Rel (Function.swap r) b a := by grind only [rel_iff]
 lemma not_rel_top_left : ¬ Rel r ⊤ a := fun h ↦ h.ne_left rfl
@@ -121,27 +118,85 @@ lemma rel_le_zero : Rel (· ≤ ·) a 0 ↔ IsZero a := by simp +contextual [rel
 lemma rel_eq_zero : Rel (· = ·) a 0 ↔ IsZero a := by simp +contextual [rel_iff, IsZero, iff_def]
 lemma rel_zero_eq : Rel (· = ·) 0 a ↔ IsZero a := by
   simp +contextual [rel_iff, IsZero, iff_def, eq_comm]
+
+
+attribute [enat_grind_canonize] rel_coe_coe rel_ofNat_coe rel_coe_ofNat rel_one_ofNat rel_ofNat_one
+rel_one_coe rel_coe_one rel_zero_le rel_zero_lt rel_one_le rel_zero_ne rel_ne_zero rel_lt_zero
+rel_lt_one rel_le_zero rel_eq_zero rel_zero_eq not_rel_top_left not_rel_top_right
+
+-- Optimizations
+
+
 lemma rel_top_mul_left {a b : ℕ∞} : Rel r (⊤ * a) b ↔ IsZero a ∧ Rel r 0 b := by
   obtain rfl | (hne : ¬ IsZero a) := eq_or_ne a 0
   · simp [isZero_zero]
   simp [top_mul hne, hne, rel_iff]
+
 lemma rel_top_mul_right {a b : ℕ∞} : Rel r a (⊤ * b) ↔ IsZero b ∧ Rel r a 0 := by
   rw [← rel_swap, rel_top_mul_left, rel_swap]
+
 lemma rel_mul_top_left {a b : ℕ∞} : Rel r (a * ⊤) b ↔ IsZero a ∧ Rel r 0 b := by
   rw [mul_comm, rel_top_mul_left]
+
 lemma rel_mul_top_right {a b : ℕ∞} : Rel r a (b * ⊤) ↔ IsZero b ∧ Rel r a 0 := by
   rw [mul_comm, rel_top_mul_right]
+
 lemma rel_top_sub_left {a b : ℕ∞} : Rel r (⊤ - a) b ↔ ¬ IsNat a ∧ Rel r 0 b := by
   cases a with
   | top => simp [not_isNat_top]
   | coe a => simp [rel_iff, isNat_iff]
-lemma rel_top_sub_right {a b : ℕ∞} : Rel r a (⊤ - b) ↔ ¬ IsNat b ∧ Rel r a 0 := by
+
+lemma rel_top_sub_right : Rel r a (⊤ - b) ↔ ¬ IsNat b ∧ Rel r a 0 := by
   rw [← rel_swap, rel_top_sub_left, rel_swap]
 
-attribute [enat_grind_canonize] rel_coe_coe rel_ofNat_coe rel_coe_ofNat rel_one_ofNat rel_ofNat_one
-rel_one_coe rel_coe_one rel_zero_le rel_zero_lt rel_one_le rel_zero_ne rel_ne_zero rel_lt_zero
-rel_lt_one rel_le_zero rel_eq_zero rel_zero_eq rel_top_mul_left rel_top_mul_right rel_mul_top_left
+lemma rel_le_add_self : Rel (· ≤ ·) b (a + b) ↔ IsNat a ∧ IsNat b := by
+  simp [rel_le_iff, isNat_iff]
+
+lemma rel_le_self_add : Rel (· ≤ ·) a (a + b) ↔ IsNat a ∧ IsNat b := by
+  rw [add_comm, rel_le_add_self, and_comm]
+
+lemma rel_le_add_right : Rel (· ≤ ·) (a + b) a ↔ IsNat a ∧ IsZero b := by
+  grind [rel_le_iff, ENat.add_le_left_iff, isNat_iff, ENat.add_eq_top, IsZero, zero_ne_top]
+
+lemma rel_le_add_left : Rel (· ≤ ·) (a + b) b ↔ IsZero a ∧ IsNat b := by
+  rw [add_comm, rel_le_add_right, and_comm]
+
+lemma rel_eq_add_right : Rel (· = ·) a (a + b) ↔ IsNat a ∧ IsZero b := by
+  grind [rel_eq_iff, ENat.add_eq_left_iff, isNat_iff, ENat.add_eq_top, IsZero, zero_ne_top]
+
+lemma rel_eq_add_left : Rel (· = ·) a (b + a) ↔ IsNat a ∧ IsZero b := by
+  rw [add_comm, rel_eq_add_right]
+
+lemma rel_add_right_eq : Rel (· = ·) (a + b) a ↔ IsNat a ∧ IsZero b := by
+  rw [add_comm, rel_eq_iff, and_comm (a := IsNat ..), eq_comm, ← rel_eq_iff, rel_eq_add_left]
+
+lemma rel_add_left_eq : Rel (· = ·) (a + b) b ↔ IsZero a ∧ IsNat b := by
+  rw [add_comm, rel_add_right_eq, and_comm]
+
+lemma rel_add_eq_add_left : Rel (· = ·) (a + b) (a + c) ↔ Rel (· = ·) b c ∧ IsNat a := by
+  grind [rel_eq_iff, ENat.add_eq_add_left_iff, isNat_iff, ENat.add_eq_top]
+
+lemma rel_add_eq_add_right : Rel (· = ·) (a + c) (b + c) ↔ Rel (· = ·) a b ∧ IsNat c := by
+  rw [add_comm a, add_comm b, rel_add_eq_add_left]
+
+lemma rel_add_le_add_left : Rel (· ≤ ·) (a + b) (a + c) ↔ Rel (· ≤ ·) b c ∧ IsNat a := by
+  grind [rel_le_iff, ENat.add_le_add_left_iff, isNat_iff, ENat.add_eq_top]
+
+lemma rel_add_le_add_right : Rel (· ≤ ·) (a + c) (b + c) ↔ Rel (· ≤ ·) a b ∧ IsNat c := by
+  rw [add_comm a, add_comm b, rel_add_le_add_left]
+
+lemma rel_add_lt_add_left : Rel (· < ·) (a + b) (a + c) ↔ Rel (· < ·) b c ∧ IsNat a := by
+  grind [rel_lt_iff, ENat.add_lt_add_left_iff, isNat_iff, ENat.add_eq_top]
+
+lemma rel_add_lt_add_right : Rel (· < ·) (a + c) (b + c) ↔ Rel (· < ·) a b ∧ IsNat c := by
+  rw [add_comm a, add_comm b, rel_add_lt_add_left]
+
+
+attribute [enat_grind_canonize] rel_top_mul_left rel_top_mul_right rel_mul_top_left
 rel_mul_top_right rel_top_sub_left rel_top_sub_right not_rel_top_left not_rel_top_right
+rel_le_add_self rel_le_self_add rel_le_add_right rel_le_add_left rel_eq_add_right rel_eq_add_left
+rel_add_right_eq rel_add_left_eq rel_add_eq_add_left rel_add_eq_add_right rel_add_le_add_left
+rel_add_le_add_right rel_add_lt_add_left rel_add_lt_add_right
 
 -- grind lemmas for `Rel` + `IsZero`. I don't think we need these for `Rel` + `IsNat`,
 -- since `Rel` is always given in conjunction with explicit `IsNat` proofs.
@@ -197,7 +252,7 @@ lemma eq_top_of_not_isNat (h : ¬ IsNat a) : a = ⊤ := by
 macro "preprocess" : tactic => `(tactic | simp only [enat_grind_presimp] at *)
 
 /-- `process` applies `enat_to_nat_top` lemmas everywhere to canonicalize `ℕ∞` equalities and
-inequalities into boolean combinations of `IsNat`, `IsZero`, and `Rel`.
+inequalities into certain boolean combinations of `IsNat`, `IsZero`, and `Rel`.
 It should use `norm_cast` to simplify coercions, but there is currently a bug with `grind`.  -/
 macro "process" : tactic => `(tactic| focus (
     (try simp only [enat_grind_canonize, ← Nat.cast_add, ← Nat.cast_mul] at *)
@@ -236,7 +291,6 @@ example (hb : a < b) : b ≠ 0 := by
   grind
 
 example (h1 : a + 2 ≤ a) : a = ⊤ := by
-  preprocess
   -- without preprocessing, this will require a case split on a.
   process
   grind
@@ -247,15 +301,29 @@ example (h1 : a + 2 ≤ a) : a = ⊤ := by
   by_cases ha : IsNat a
   · obtain ⟨a, rfl⟩ := ha
     process
-    omega
   grind
 
 example (h1 : a + b ≤ a + c) (h2 : d + c ≤ d) (ha : a < ⊤) (hd : d ≠ ⊤) : b = 0 := by
-  preprocess
   process
   obtain rfl : IsZero c := by grind
   process
   grind
+
+example (h1 : a + b + c = c + a) (h2 : 1 ≤ b) : a = ⊤ ∨ c = ⊤ := by
+  process
+  cases a with
+  | top => process
+  | coe a =>
+  process
+  cases c with
+  | top => process
+  | coe c =>
+  process
+  cases b with
+  | top => process
+  | coe b =>
+  process
+  omega
 
 example (h1 : a + b * c ≠ ⊤) (hc : 1 ≤ c) : b ≠ ⊤ := by
   process
