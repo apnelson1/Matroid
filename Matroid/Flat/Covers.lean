f4af8a76bfaac9cd92599bfb0753f2917a044dbe
fair8 <118382318+fair8@users.noreply.github.com>
2026-02-06 13:41:25 -0500

    Nwip

---
+++ Matroid/Flat/Covers.lean
@@ -0,0 +1,7 @@
+import Matroid.Minor.Rank
+import Mathlib.Tactic.Linarith
+
+variable {α : Type*} {M : Matroid α} {I F X Y F' F₀ F₁ F₂ P L H H₁ H₂ H' B C K : Set α} {e f : α}
+
+open Set
+namespace Matroid
