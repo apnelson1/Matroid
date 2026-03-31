ce294e73733c172fabf9ad447e91556da1493b17
dillon128 <dillonmayhew@gmail.com>
2026-03-31 12:50:06 +0100

    More WIP in the Cretaceous branch of the Splitter Theorem

--- Matroid/Connectivity/Splitter/Cretaceous.lean
+++ Matroid/Connectivity/Splitter/Cretaceous.lean
@@ -16,7 +16,7 @@ open Set Matroid Function Separation
 
 namespace Matroid
 
-variable {α β : Type*} {e f x y : α} {X Y : Set α} {i j : Bool} {k : ℕ∞}
+variable {α β : Type*} {e f x y : α} {X Y C₁ C₂: Set α} {i j k : Bool} {k : ℕ∞}
     {M : Matroid α} {N : Matroid β} {P : M.Separation}
 
 theorem splitter_theorem (hM : M.TutteConnected 3) (hN : N.TutteConnected 3) (fNM : N <i M)
@@ -105,6 +105,7 @@ This should be useful, and can be found with the lemma
 -- = mapEquiv (M ／ X ＼ ((P i) \ (insert x X))) (Equiv.swap x y) := by
 
 /-- Replace this with `Dep.of_isMinor_of_subset`. -/
+
 lemma Dep.of_minor_disjoint {N : Matroid α} (hNM : N ≤m M) (hX : X ⊆ N.E) :
     M.Dep X → N.Dep X := by
   apply IsMinor.exists_contract_indep_delete_coindep at hNM
@@ -132,9 +133,77 @@ lemma Separation.contract_exists_disjoint_base_of_eConn_eq_one {N : Matroid α}
   have hNe := hN.isNonloop_of_mem <| show e ∈ N.E by grind [he.symm.subset]
   simpa [he] using hNe.of_isMinor hNM.dual
 
+lemma Separation.isCircuit_union_inter_of_eConn_le_one_bool {C : Bool → Set α}
+    (hC : ∀ j, M.IsCircuit (C j)) (hP : P.eConn ≤ 1) (hCP : ∀ i j, (C i ∩ P j).Nonempty) :
+    ∀ i j k, M.IsCircuit ((C j ∩ P i) ∪ (C k ∩ P !i)) := by
+  intro i j k
+  by_cases! aux₁ : j = k
+  · rw [← aux₁]
+    have h : (C j ∩ P i ∪ C j ∩ P !i) = C j := by
+      rw [← inter_union_distrib_left, Separation.union_bool_eq, inter_eq_self_of_subset_left]
+      exact (hC j).subset_ground
+    rw [h]
+    exact hC j
+  · rw [← Bool.not_eq] at aux₁
+    rw [← aux₁]
+    by_cases! aux₂ : j = i
+    · rw [aux₂, inter_comm, inter_comm (C !i)]
+      have hwin₁ := P.isCircuit_iUnion_inter_of_eConn_le_one (hC) (hP) (hCP)
+      rwa [Set.iUnion_bool' _ i] at hwin₁
+    · rw [← Bool.not_eq, Bool.not_eq_eq_eq_not] at aux₂
+      rw [aux₂, Bool.not_not]
+      nth_rw 2 [← Bool.not_not i]
+      rw [← Separation.symm_apply, ← Separation.symm_apply P, union_comm,
+        inter_comm, inter_comm (C !i)]
+      have hCP₂ : ∀ (i j : Bool), (C i ∩ P.symm j).Nonempty := by
+        intro i j
+        rw [Separation.symm_apply P j]
+        exact hCP i !j
+      have hwin₂ := P.symm.isCircuit_iUnion_inter_of_eConn_le_one (hC) (by simpa) (hCP₂)
+      rwa [Set.iUnion_bool' _ i] at hwin₂
+
+lemma Separation.unique_circuit_of_eConn_le_one {C : Bool → Set α}
+    (hC : ∀ j, (M ↾ (X ∪ (P i))).IsCircuit (C j)) (hX : (M ↾ (P !i)).Indep X)
+    (hPconn : P.eConn ≤ 1) : ((C true) ∩ X) = ((C false) ∩ X) := by
+  rw [restrict_indep_iff] at hX
+  have hC₁ : ∀ j, M.IsCircuit (C j) ∧ (C j) ⊆ X ∪ (P i):= by
+    intro j
+    specialize hC (j := j)
+    rw [restrict_isCircuit_iff] at hC
+    exact hC
+  clear hC
+  have hePi : ∀ j, ((C j) ∩ (P i)).Nonempty := by
+    by_contra! hc
+    obtain ⟨j, hj⟩ := hc
+    have h : C j ⊆ X := by
+      specialize hC₁ (j := j)
+      rw [← disjoint_iff_inter_eq_empty] at hj
+      grind
+    rw [indep_iff_forall_subset_not_isCircuit] at hX
+    grind
+  have heX : ∀ j, (C j) ∩ X = ∅ → (C !j) ∩ X = ∅ := by
+    intro j he
+    by_contra! h
+
+    sorry
+  sorry
+
 lemma IsMinor.contract_disjoint_base_of_eConn_eq_one {N : Matroid α} (hPconn : P.eConn ≤ 1)
-    (hN : N.TutteConnected 3) (hNM : N ≤m M) (hPi: (N.E ∩ (P !i)).Subsingleton)
-    (hX : (M ／ (P i)).IsBase X) :  Disjoint X N.E → N ≤m (M ／ X) := by
+    (hN : N.TutteConnected 3) (hNM : N ≤m M) (hPi: (N.E ∩ (P !i)).Subsingleton) :
+    ∃ X, (M ／ (P i)).IsBase X ∧ N ≤m (M ／ X) := by
+
+  /-
+  Let x be a subsingleton set such that (P !i) \ x is disjoint with N.E.
+  Let C, D be a partition of (P !i) \ x such that C is independent, D is dependent, and
+  N is a minor of M / C \ D.
+  The Separation.unique_circuit_of_eConn_le_one lemma implies that there is a subsingleton
+  set y such that C \ y is independent in M / P i.
+  Let B be a basis of M / P i \ x that contains C \ y.
+  We want to show that we can contract B from M and obtain an N minor.
+  We accomplish this by showing that in M / (C \ y), we have skewness between B \ C and P i,
+  so we are free to contract B \ C instead of deleting it.
+  -/
+
   sorry
 
 lemma splitter_no_triangle (hM : M.TutteConnected 3) (hN : N.TutteConnected 3) (fNM : N <i M)
@@ -157,99 +226,104 @@ lemma splitter_no_triangle (hM : M.TutteConnected 3) (hN : N.TutteConnected 3) (
   have henot3con : ¬ (M ＼ {e}).TutteConnected (2 + 1) := (hcon e).1 hed
   rw [not_tutteConnected_iff_exists] at henot3con
   obtain ⟨P, hP⟩ := henot3con
-  --- At this point we want to show that P has a "small side" relative to a copy of N,
-  --- that is, for a fixed copy of N, there is a side of P that contains at most one
-  --- element of that copy. Now this small side cannot have rank 1, because M is simple.
-  --- It cannot have corank 1, because then e would be in a triad, and e is a
-  --- deletable element. So by a lemma, the small side contains a "flexible" element, f, meaning
-  --- that both M \ f and M / f have isomorphic copies of N. The hypotheses imply
-  --- f is in no triangle and in no triad, so that si(M/f) = M/f and co(M\f) = M\f.
-  --- Now we can apply Bixby to f and deduce that the splitter theorem holds.
-  sorry
 
+  /-
+  We are under the hypotheses that if M/e has an N-minor, then e is not in a triangle, and
+  if M\e has an N-minor, then e is not in a triad. The other case leads to 4-element fans.
+
+  At this point we have used duality to assume that there is an element e such that
+  M\e has an N-minor. Note that e is not in a triad. If M\e is 3-connected, then we are done,
+  so we assume that M\e has a 2-separation P.
+
+  We show that P has a "small side" relative to a copy of N, that is, for a fixed copy
+  of N, there is a side of P that contains at most one element of that copy. This small
+  side cannot have rank 1, because M is simple. It cannot have corank 1, because then e
+  would be in a triad, and e is a deletable element. So by a lemma, the small side contains
+  a "flexible" element, f, meaning that both M \ f and M / f have isomorphic copies of N.
+  The hypotheses imply f is in no triangle and in no triad, so that si(M/f) = M/f and
+  co(M\f) = M\f. Now we can apply Bixby to f and deduce that the splitter theorem holds.
+  -/
+
+  sorry
 
 --  Scraps from this point onward
 
---
+/-
 
--- lemma IsMinor.exists_smallside_of_eConn_eq_zero {N : Matroid α} (hNM : N ≤m M)
---     (hN : N.TutteConnected 2) (hPconn : P.eConn = 0) :
---     ∃ i, (P i ∩ N.E) = ∅ := by
---   by_contra! hcon₁
---   have hNnonem : N.Nonempty := by
---     by_contra! hcon₂
---     rw [← ground_nonempty_iff, not_nonempty_iff_eq_empty] at hcon₂
---     rw [hcon₂] at hcon₁
---     simp_all only [inter_empty, Set.not_nonempty_empty, forall_const]
---   have hj : ∀ j : Bool, ∃ e, e ∈ (P j ∩ N.E) := by
---     intro j
---     specialize hcon₁ (i := j)
---     rw [nonempty_def] at hcon₁
---     obtain ⟨e, he⟩ := hcon₁
---     use e
---   have hNconn : (P.induce hNM.subset).eConn = 0 := by
---     rw [← ENat.lt_one_iff_eq_zero, show (1 : ℕ∞) = 0 + 1 from rfl, ENat.lt_add_one_iff]
---     grw [eConn_induce_le_of_isMinor _ hNM, hPconn]
---     simp
---   rw [tutteConnected_two_iff] at hN
---   have hNP := hN.trivial_of_eConn_eq_zero hNconn
---   rw [Separation.trivial_def, induce_apply, induce_apply] at hNP
---   obtain hPf | hPt := hNP
---   specialize hj (j := false)
---   grind
---   specialize hj (j := true)
---   grind
+lemma IsMinor.exists_smallside_of_eConn_eq_zero {N : Matroid α} (hNM : N ≤m M)
+    (hN : N.TutteConnected 2) (hPconn : P.eConn = 0) :
+    ∃ i, (P i ∩ N.E) = ∅ := by
+  by_contra! hcon₁
+  have hNnonem : N.Nonempty := by
+    by_contra! hcon₂
+    rw [← ground_nonempty_iff, not_nonempty_iff_eq_empty] at hcon₂
+    rw [hcon₂] at hcon₁
+    simp_all only [inter_empty, Set.not_nonempty_empty, forall_const]
+  have hj : ∀ j : Bool, ∃ e, e ∈ (P j ∩ N.E) := by
+    intro j
+    specialize hcon₁ (i := j)
+    rw [nonempty_def] at hcon₁
+    obtain ⟨e, he⟩ := hcon₁
+    use e
+  have hNconn : (P.induce hNM.subset).eConn = 0 := by
+    rw [← ENat.lt_one_iff_eq_zero, show (1 : ℕ∞) = 0 + 1 from rfl, ENat.lt_add_one_iff]
+    grw [eConn_induce_le_of_isMinor _ hNM, hPconn]
+    simp
+  rw [tutteConnected_two_iff] at hN
+  have hNP := hN.trivial_of_eConn_eq_zero hNconn
+  rw [Separation.trivial_def, induce_apply, induce_apply] at hNP
+  obtain hPf | hPt := hNP
+  specialize hj (j := false)
+  grind
+  specialize hj (j := true)
+  grind
 
---
+lemma IsMinor.exists_smallside_of_eConn_eq_one {N : Matroid α} (hNM : N ≤m M)
+    (hN : N.TutteConnected 3) (hPcon : P.eConn = 1) :
+    ∃ i, (P i ∩ N.E).Subsingleton := by
+  by_contra! hc
+  rw [show (3 : ℕ∞) = 2 + 1 by grind, tutteConnected_iff_forall] at hN
+  specialize hN (P:= P.induce hNM.subset)
+  have h : (P.induce hNM.subset).eConn + 1 ≤ 2 := by
+    grw [eConn_induce_le_of_isMinor P]
+    · apply le_of_eq at hPcon
+      rw [← ENat.add_one_le_add_one_iff, one_add_one_eq_two] at hPcon
+      exact hPcon
+    · exact hNM
+  apply hN at h
+  rw [isTutteSeparation_iff_lt_encard] at h
+  · push_neg at h
+    obtain ⟨i, hi⟩ := h
+    specialize hc (i:= i)
+    grw [eConn_induce_le_of_isMinor P, hPcon] at hi
+    · rw [← Set.one_lt_encard_iff_nontrivial] at hc
+      rw [← induce_apply P hNM.subset] at hc
+      grw [hi] at hc
+      tauto
+    · exact hNM
+  · rw [← lt_top_iff_ne_top]
+    grw [eConn_induce_le_of_isMinor, hPcon]
+    · simp [ENat.one_lt_top]
+    · exact hNM
 
--- lemma IsMinor.exists_smallside_of_eConn_eq_one {N : Matroid α} (hNM : N ≤m M)
---     (hN : N.TutteConnected 3) (hPcon : P.eConn = 1) :
---     ∃ i, (P i ∩ N.E).Subsingleton := by
---   by_contra! hc
---   rw [show (3 : ℕ∞) = 2 + 1 by grind, tutteConnected_iff_forall] at hN
---   specialize hN (P:= P.induce hNM.subset)
---   have h : (P.induce hNM.subset).eConn + 1 ≤ 2 := by
---     grw [eConn_induce_le_of_isMinor P]
---     · apply le_of_eq at hPcon
---       rw [← ENat.add_one_le_add_one_iff, one_add_one_eq_two] at hPcon
---       exact hPcon
---     · exact hNM
---   apply hN at h
---   rw [isTutteSeparation_iff_lt_encard] at h
---   · push_neg at h
---     obtain ⟨i, hi⟩ := h
---     specialize hc (i:= i)
---     grw [eConn_induce_le_of_isMinor P, hPcon] at hi
---     · rw [← Set.one_lt_encard_iff_nontrivial] at hc
---       rw [← induce_apply P hNM.subset] at hc
---       grw [hi] at hc
---       tauto
---     · exact hNM
---   · rw [← lt_top_iff_ne_top]
---     grw [eConn_induce_le_of_isMinor, hPcon]
---     · simp [ENat.one_lt_top]
---     · exact hNM
+lemma exists_deletable_contractible_of_smallside {N : Matroid α} (hNM : N ≤m M)
+    (hN : N.TutteConnected 3) (hPcon : P.eConn = 1) (hPN : (P i ∩ N.E).Subsingleton)
+    (hPr : 2 ≤ M.eRk (P i)) (hPcr : 2 ≤ M✶.eRk (P i)) :
+    ∃ e ∈ P i, M.IsDeletable N e ∧ M.IsContractible N e := by
+  have hN2 : N.TutteConnected 2 := hN.mono (by norm_num)
+  by_contra! hcon₁
+  have hM : M.TutteConnected (1 + 1) := by
+    by_contra! hcon₂
+    rw [not_tutteConnected_iff_exists] at hcon₂
+    obtain ⟨Q, hQ⟩ := hcon₂
+    nth_rw 2 [show (1 : ℕ∞) = 0 + 1 from rfl] at hQ
+    rw [ENat.add_one_le_add_one_iff] at hQ
+    sorry
+  sorry
 
---
+lemma IsMinor.preserve_minor_of_eConn_eq_one {N : Matroid α} (hNM : N ≤m M) (hM : M.Connected)
+    (hN : N.TutteConnected 3) (hP : P.IsTutteSeparation) (hPcon : P.eConn = 1)
+    (hPN : (P i ∩ N.E).Subsingleton) : ∀ e ∈ P i, ((M ＼ {e}).Connected → M.IsDeletable N e) ∧
+    ((M ／ {e}).Connected → M.IsContractible N e) := by sorry
 
--- lemma exists_deletable_contractible_of_smallside {N : Matroid α} (hNM : N ≤m M)
---     (hN : N.TutteConnected 3) (hPcon : P.eConn = 1) (hPN : (P i ∩ N.E).Subsingleton)
---     (hPr : 2 ≤ M.eRk (P i)) (hPcr : 2 ≤ M✶.eRk (P i)) :
---     ∃ e ∈ P i, M.IsDeletable N e ∧ M.IsContractible N e := by
---   have hN2 : N.TutteConnected 2 := hN.mono (by norm_num)
---   by_contra! hcon₁
---   have hM : M.TutteConnected (1 + 1) := by
---     by_contra! hcon₂
---     rw [not_tutteConnected_iff_exists] at hcon₂
---     obtain ⟨Q, hQ⟩ := hcon₂
---     nth_rw 2 [show (1 : ℕ∞) = 0 + 1 from rfl] at hQ
---     rw [ENat.add_one_le_add_one_iff] at hQ
---     sorry
---   sorry
-
---
-
--- lemma IsMinor.preserve_minor_of_eConn_eq_one {N : Matroid α} (hNM : N ≤m M) (hM : M.Connected)
---     (hN : N.TutteConnected 3) (hP : P.IsTutteSeparation) (hPcon : P.eConn = 1)
---     (hPN : (P i ∩ N.E).Subsingleton) : ∀ e ∈ P i, ((M ＼ {e}).Connected → M.IsDeletable N e) ∧
---     ((M ／ {e}).Connected → M.IsContractible N e) := by sorry
+-/
