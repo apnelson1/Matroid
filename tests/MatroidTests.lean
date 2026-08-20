7f609529d187fc60640cd6c2b1ad1bb83293ddf6
Jun2M <rnjsgurwns27@gmail.com>
2026-08-19 23:27:09 -0400

    quick fix

--- tests/MatroidTests.lean
+++ tests/MatroidTests.lean
@@ -4,4 +4,3 @@ public import MatroidTests.Iso
 public import MatroidTests.GrindTags
 public import MatroidTests.JordanCurve
 public import MatroidTests.PolygonalPath
-public import MatroidTests.TopologicalMinor
